d325e7740237a94c06e18376f520adf2ce454367
LIAOJIANS <272781702@qq.com>
2019-08-25 13:34:48 +0800

    代理不成搞跨域

--- app.js
+++ app.js
@@ -37,5 +37,22 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+//解决跨域
+// app.all('*',function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   if (req.method == 'OPTIONS') {
+//     res.send(200);
+//   }
+//   else {
+//     next();
+//   }
+// });
+// var server = app.listen(3000, function () {
+//   var host = server.address().address
+//   var port = server.address().port
+//   console.log("应用实例，访问地址为 http://%s:%s", host, port)
+//
+// })
 module.exports = app;
